0731e375decb127a9b09bf92e12a4dc97664db51
XuBenHao <xbh370970843@163.com>
2020-04-03 00:59:45 +0800

    排序
    排序--快速排序

---
+++ stdafx.h
@@ -0,0 +1,26 @@
+#ifndef ALGORITHM_STDAFX_H
+#define ALGORITHM_STDAFX_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <malloc.h>
+#include <iostream>
+#include <bitset>
+#include <time.h>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <math.h>
+#include <vector>
+
+//#include "External/Language/Language.h"
+//#include "External/Math/Math.h"
+//#include "External/PlaneGeometry/PlaneGeometry.h"
+
+#ifdef ALGORITHMLIB
+#else
+#define ALGORITHMLIB __declspec(dllimport)
+#endif 
+#endif
+
