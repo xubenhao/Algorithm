eb4a0e8b2c119b236740d8c2a66cb00c2ac9a3ae
XuBenHao <xbh370970843@163.com>
2020-04-30 22:28:00 +0800

    非负权重图单源最短路径
    非负权重图单源最短路径完整实现

--- Algorithm/Graph/ShorestPath.h
+++ Algorithm/Graph/ShorestPath.h
@@ -77,8 +77,11 @@ namespace AlLib
 				~ShorestPath();
 
 				DataStruct::Array::DynArray<Node*> Run(const Key& nSourceKey_);
-				/*DataStruct::Array::DynArray<Node*> RunForWeightNoNegtive(const Key& nSourceKey_);
-				DataStruct::Array::DynArray<Node*> RunForNoCircle(const Key& nSourceKey_);*/
+				DataStruct::Array::DynArray<Node*> RunForWeightNoNegtive(const Key& nSourceKey_);
+				/*DataStruct::Array::DynArray<Node*> RunForNoCircle(const Key& nSourceKey_);*/
+			private:
+				ShorestPath(const ShorestPath&) = default;
+				ShorestPath& operator=(const ShorestPath&) = default;
 			private:
 				const InnerGraph& m_nGraph;
 				InnerTree m_nNodeMappingTree;
@@ -112,7 +115,12 @@ namespace AlLib
 			template<typename Key, typename Value>
 			ShorestPath<Key, Value>::~ShorestPath()
 			{
-
+				DataStruct::Array::DynArray<InnerTree::Pair> _arrTreePairs = m_nNodeMappingTree.GetArray();
+				for (int _i = 0; _i < _arrTreePairs.GetSize(); _i++)
+				{
+					delete (_arrTreePairs[_i].m_nValue);
+					_arrTreePairs[_i].m_nValue = nullptr;
+				}
 			}
 
 			template<typename Key, typename Value>
@@ -201,6 +209,94 @@ namespace AlLib
 				
 				return _arrpNodes;
 			}
+
+			template<typename Key, typename Value>
+			DataStruct::Array::DynArray<typename ShorestPath<Key, Value>::Node*> ShorestPath<Key, Value>::RunForWeightNoNegtive(const Key& nSourceKey_)
+			{
+				InnerGraph::Node* _pGraphNode = m_nGraph.SearchNode(nSourceKey_);
+				if (_pGraphNode == nullptr)
+				{
+					throw "can not find key in graph";
+				}
+
+				DataStruct::Array::DynArray<Node*> _arrpNodes;
+				DataStruct::Array::DynArray<Node*> _arrpRetNodes;
+				DataStruct::Array::DynArray<InnerTree::Pair>_arrTreePairs = m_nNodeMappingTree.GetArray();
+				for (int _i = 0; _i < _arrTreePairs.GetSize(); _i++)
+				{
+					_arrpNodes.Add(_arrTreePairs[_i].m_nValue);
+					_arrpNodes[_i]->Reset();
+				}
+
+				DataStruct::Array::DynArray<typename InnerGraph::Node*> _arrGraphNodes = m_nGraph.GetNodesArray();
+				DataStruct::Array::DynArray<typename InnerGraph::Edge*> _arrGraphEdges = m_nGraph.GetEdgesArray();
+				while (_arrpNodes.GetSize() > 0)
+				{
+					Node* _pNode = nullptr;
+					// 从节点集合1选出距离最短的Node
+					for (int _i = 0; _i < _arrpNodes.GetSize(); _i++)
+					{
+						if (_arrpNodes[_i]->m_pPreNode != nullptr
+							|| _arrpNodes[_i]->m_pGraphNode == _pGraphNode)
+						{
+							if (_pNode == nullptr
+								|| _pNode->m_nDistance > _arrpNodes[_i]->m_nDistance)
+							{
+								_pNode = _arrpNodes[_i];
+							}
+						}
+					}
+
+					
+					if (_pNode != nullptr)
+					{
+						// 选出的Node放入另一Node集合
+						_arrpNodes.DeleteByValue(_pNode);
+						_arrpRetNodes.Add(_pNode);
+						// 对所有该节点可达节点
+						DataStruct::Array::DynArray<Key> _arrDestinations = _pNode->m_pGraphNode->GetDests();
+						for (int _i = 0; _i < _arrDestinations.GetSize(); _i++)
+						{
+							Node* _pDestNode = nullptr;
+							m_nNodeMappingTree.Search(_arrDestinations[_i], _pDestNode);
+							if (_pDestNode == nullptr)
+							{
+								throw "cannot find dest in graph";
+							}
+
+							InnerGraph::EdgeIdentity _nIdentity;
+							_nIdentity.m_nStartKey = _pNode->m_pGraphNode->GetPair().m_nKey;
+							_nIdentity.m_nEndKey = _arrDestinations[_i];
+							InnerGraph::Edge* _pEdge = m_nGraph.SearchEdge(_nIdentity);
+							if (_pEdge == nullptr)
+							{
+								throw "cannot find edge";
+							}
+
+							// 对可达节点进行松弛判断
+							if ((_pDestNode->m_pPreNode == nullptr && _pDestNode->m_pGraphNode != _pGraphNode)
+								|| _pDestNode->m_nDistance > _pNode->m_nDistance + _pEdge->m_nWeight)
+							{
+								_pDestNode->m_pPreNode = _pNode;
+								_pDestNode->m_nDistance = _pNode->m_nDistance + _pEdge->m_nWeight;
+							}
+						}
+					}
+					// 选不出来，表示剩余Node均为不可达的
+					else
+					{
+						for (int _i = 0; _i < _arrpNodes.GetSize(); _i++)
+						{
+							_arrpRetNodes.Add(_arrpNodes[_i]);
+						}
+
+						_arrpNodes.DeleteAll();
+					}
+				}
+
+				return _arrpRetNodes;
+			}
+
 		}
 	}
 }
